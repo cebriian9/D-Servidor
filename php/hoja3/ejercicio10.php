a9be4a2b62cfbc21c0e4c23ad1d90d997381de6b
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2022-10-11 10:04:16 +0200

    a

--- php/hoja3/ejercicio10.php
+++ php/hoja3/ejercicio10.php
@@ -1,6 +1,6 @@
 <?php
 
 //echo (<a href= "/arbol/prueba.php" class="prueba" onmouseOver="status='hola';
-//return trae;">pruebade\enlace</a>) buscar como se hace*
+//return trae;">pruebade\enlace</a>)
 
 ?>
