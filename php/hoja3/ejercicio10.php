d57c42bebcd46fc41a17c386a41d067892c138e2
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2022-10-11 10:34:10 +0200

    Add files via upload

--- php/hoja3/ejercicio10.php
+++ php/hoja3/ejercicio10.php
@@ -1,6 +1,9 @@
 <?php
+$frase="<a href= \"/arbol/prueba.php\" class=\"prueba\" onmouseOver=\"status='hola';
+return trae;\">pruebade\\enlace</a>";
 
-//echo (<a href= "/arbol/prueba.php" class="prueba" onmouseOver="status='hola';
-//return trae;">pruebade\enlace</a>)
+print(htmlentities($frase));
+
+
 
 ?>
