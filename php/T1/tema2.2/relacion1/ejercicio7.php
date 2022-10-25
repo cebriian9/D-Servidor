4f74a2e6f762794112d329da9a5f4dd2f5e6ee80
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2022-10-25 12:29:46 +0200

    terminada relacion 1 funciones
    a tomar por culo

---
+++ php/T1/tema2.2/relacion1/ejercicio7.php
@@ -0,0 +1,20 @@
+<?php
+$numero=2;
+print("nuemero: $numero<br>");
+cuadrado($numero);
+cubo($numero);
+
+function cuadrado($numero)
+{
+    $resultado=pow($numero,2);
+
+    print("cuadrado: $resultado<br>");
+}
+
+function cubo($numero)
+{
+    $resultado=pow($numero,3);
+
+    print("cubo: $resultado");
+}
+?>
