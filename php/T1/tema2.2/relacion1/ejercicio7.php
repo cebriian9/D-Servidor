dbcd96a7345452254353ed96fecbabc74f5a6afc
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2022-11-07 10:53:47 +0100

    Update ejercicio7.php

--- php/T1/tema2.2/relacion1/ejercicio7.php
+++ php/T1/tema2.2/relacion1/ejercicio7.php
@@ -7,12 +7,14 @@ cubo($numero);
 function cuadrado($numero)
 {
     $resultado=pow($numero,2);
+
     print("cuadrado: $resultado<br>");
 }
 
 function cubo($numero)
 {
     $resultado=pow($numero,3);
+    
     print("cubo: $resultado");
 }
 ?>
