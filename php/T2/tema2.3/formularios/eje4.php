521f99acd07edf9550f02f3ce24cc4ef3471f327
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2023-01-17 12:55:15 +0100

    Update eje4.php

--- php/T2/tema2.3/formularios/eje4.php
+++ php/T2/tema2.3/formularios/eje4.php
@@ -1,25 +1,39 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>eje4</title>
 </head>
+
 <body>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <label for="titulo"><b>titulo*</b></label>
-    <input type="text" name="titulo" id="titulo"><br>
-    
+        <label for="titulo"><b>titulo*</b></label>
+        <input type="text" name="titulo" id="titulo"><br>
 
-    <label for="noticia"><b>contenido</b></label><br>
-    <textarea name="noticia" id="noticia" cols="30" rows="10" placeholder="Inserte noticia"></textarea><br>
+        <?php
 
-    <label for="imagen"><b>imagen</b></label>
-    <input type="file" name="imagen" id="imagen"><br>
+        if (isset($_REQUEST['enviar']) && empty(trim($_REQUEST['titulo'])))
+            echo ("<span style='color:red;'> Debes introducir un titulo!! </span> <br>");
+        ?>
 
-    <input type="submit" value="enviar">
+        <label for="noticia"><b>contenido</b></label><br>
+        <textarea name="noticia" id="noticia" cols="30" rows="10" placeholder="Inserte noticia"></textarea><br>
+
+        <?php
+        if (isset($_REQUEST['enviar']) && empty(trim($_REQUEST['noticia'])))
+            echo ("<span style='color:red;'> Debes introducir el contenido!! </span> <br>");
+        ?>
+
+
+        <label for="imagen"><b>imagen</b></label>
+        <input type="file" name="imagen" id="imagen"><br>
+
+        <input type="submit" value="enviar" name="enviar">
     </form>
 
 </body>
+
 </html>
