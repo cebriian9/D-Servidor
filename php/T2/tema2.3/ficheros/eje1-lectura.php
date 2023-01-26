0030d9d42ac3070ae0554992154ae4297d8f230f
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2023-01-26 13:38:19 +0100

    s

--- php/T2/tema2.3/ficheros/eje1-lectura.php
+++ php/T2/tema2.3/ficheros/eje1-lectura.php
@@ -1,5 +1,20 @@
-<?php 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eje1lectura</title>
+</head>
+<body>
+    
+<?php
+
+$fic_original = fopen("../formularios/eje1.php", "r");
+$fich_salida= fopen("/salidas/salida1.txt","w");
 
 
 
 ?>
+</body>
+</html>
