d3da03f74fed64fea45e33970c516e91d95df1bc
cebriian95 <91868841+cebriian9@users.noreply.github.com>
2023-01-27 13:45:08 +0100

    ficheros

---
+++ php/T2/tema2.3/ficheros/eje4.2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eje4.2</title>
+</head>
+<body>
+    <?php
+
+    $nombre=$_REQUEST['nombre'];
+    $comentario=$_REQUEST['comentario'];
+
+    $file=fopen("datos.txt","a");
+    
+    if (!$file) {
+        die("error al abrir");
+    }
+
+    $dato="---------\nNombre: $nombre \nComentario: $comentario \n---------\n";
+
+    fwrite($file,$dato);
+
+    echo "Los datos se guardaron chido";
+    print ($dato);
+
+    fclose($file);
+    ?>
+
+    <a href="eje4.3.php">[Ver fichero completo]</a>
+</body>
+</html>
--- php/T2/tema2.3/ficheros/eje4.php
+++ php/T2/tema2.3/ficheros/eje4.php
@@ -1,30 +1,25 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>eje4</title>
 </head>
+
 <body>
-    <form action="#" method="post">
+    <form action="eje4.2.php" method="post">
         <p>nombre</p>
         <input type="text" name="nombre">
 
         <p>comentario:</p>
         <textarea name="comentario" id="na" cols="30" rows="10"></textarea>
-<br>
+        <br>
         <input type="submit" value="enviar" name="enviar">
     </form>
 
-    <?php
-        $file=fopen("comentarios.txt", "r+");
-
-        if (!$file) {
-            die("Error al abrir");
-        }
-
-        fclose($file);
-    ?>
+    
 </body>
+
 </html>
